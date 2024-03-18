cfae9361c8348a0d349842b00578c16b3099a906
ilo80 <ilann.mimet@gmail.com>
2024-03-18 18:56:14 +0100

    Assignation des animations selon les touches pressées.
    - Animation "idle" lorsqu'aucune touche n'est pressée
    - Animation "run" lorsque la touche "Q" ou "D" est pressée (le joueur se déplace)

--- Assets/Sprites/Player.cs
+++ Assets/Sprites/Player.cs
@@ -5,10 +5,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 2f; // Vitesse de déplacement du joueur
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,12 +22,20 @@ public class PlayerMovement : MonoBehaviour
             Vector2 movement = new Vector2(moveHorizontal + 1, moveVertical);
             rb.velocity = movement * speed;
 
+            anim.SetBool("run", true);
+
         } else if (Input.GetKey(KeyCode.A)) { // La touche Q permet de déplacer vers la gauche
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
 
             Vector2 movement = new Vector2(moveHorizontal - 1, moveVertical);
             rb.velocity = movement * speed;
+
+            anim.SetBool("run", true);
+
+            
+        } else {
+            anim.SetBool("run", false);
         }
 
     }
