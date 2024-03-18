0897b142858e8f05f0447bff9fcd35d2c1041046
ilo80 <ilann.mimet@gmail.com>
2024-03-18 17:43:33 +0100

    Changement vitesse joueur LEVEL2

--- Assets/Sprites/Player.cs
+++ Assets/Sprites/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f; // Vitesse de déplacement du joueur
+    public float speed = 2f; // Vitesse de déplacement du joueur
 
     private Rigidbody2D rb;
 
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
 
             Vector2 movement = new Vector2(moveHorizontal + 1, moveVertical);
             rb.velocity = movement * speed;
+
         } else if (Input.GetKey(KeyCode.A)) { // La touche Q permet de déplacer vers la gauche
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
