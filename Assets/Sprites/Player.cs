3fe44b5e1e368fe3bde02888800daf6d2f5aa3f2
ilo80 <ilann.mimet@gmail.com>
2024-03-19 14:06:39 +0100

    Fix gravité + saut
    - La gravitée n'est désormais plus impactée par le mouvement.
    - Le saut fonctionne désormais correctement avec un déplacement.

--- Assets/Sprites/Player.cs
+++ Assets/Sprites/Player.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 2f; // Vitesse de déplacement du joueur
+    public float playerSpeed = 2f; // Vitesse de déplacement du joueur
+    public float playerJumpForce = 5f; // Force de saut du joueur
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -17,38 +19,29 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
         if (Input.GetKey(KeyCode.D)) { // la touche D permet de se déplacer  vers la droite
             moveHorizontal += 1; 
-            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-            rb.velocity = movement * speed;
 
         } else if (Input.GetKey(KeyCode.A)) { // La touche Q permet de déplacer vers la gauche
             moveHorizontal -= 1;
-            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-            rb.velocity = movement * speed;
         }
         
         if (Input.GetKey(KeyCode.Space) && grounded) { // La touche espace permet de sauter
-            moveVertical += 2;
-            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-            rb.velocity = movement * speed;
+            rb.velocity = new Vector2(rb.velocity.x, playerJumpForce);
 
             grounded = false;
-        } 
+
+        }
+
+        rb.velocity = new Vector2(moveHorizontal * playerSpeed, rb.velocity.y);
 
         anim.SetBool("run", moveHorizontal != 0);
-        anim.SetBool("jump", moveVertical != 0);
+        anim.SetBool("jump", !grounded);
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Ground") {
-            grounded = true;
-        }
+        grounded = collision.gameObject.CompareTag("Ground");
     }
 }
