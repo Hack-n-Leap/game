cca8b0927e1ecc0545f4d3dce2138305aaa0119c
ilo80 <ilann.mimet@gmail.com>
2024-04-07 19:02:40 +0200

    Nettoyage du code

--- Assets/Scripts/Windows/InputText.cs
+++ Assets/Scripts/Windows/InputText.cs
@@ -4,23 +4,8 @@ using UnityEngine;
 
 public class InputText : MonoBehaviour
 {
-    private string input;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ReadInput(string inp)
     {
-        input = inp;
-        Debug.Log(input);
+        Debug.Log(inp);
     }
 }
