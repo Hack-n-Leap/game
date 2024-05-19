c2c3ba6bb864ffc466ecfd71c63aa5f846809c7b
ilo80 <ilann.mimet@gmail.com>
2024-05-19 16:05:11 +0200

    Ajout de l'enregistrement des niveaux

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -7,39 +6,37 @@ public class GameManager : MonoBehaviour
     private SaveSystem saveSystem;
     private Rigidbody2D rb;
     public GameData gameData;
+    private static bool hasLoadedGameData = false;
 
 
     void Start()
     {
-        // Initialisation saveSystem et player
         saveSystem = gameObject.AddComponent<SaveSystem>();
+        rb = GetComponent<Rigidbody2D>();
 
-        if (saveSystem == null)
-        {
-            Debug.LogError("SaveSystem is not found in the scene.");
-            return;
-        }
+        if (!hasLoadedGameData) {
+            hasLoadedGameData = true;
 
-        gameData = saveSystem.LoadGame(); // Chargement de la sauvegarde
+            if (saveSystem == null)
+            {
+                Debug.LogError("SaveSystem is not found in the scene.");
+                return;
+            }
 
-        if (gameData == null) { // Cas où aucune sauvegarde n'existe
-            gameData = new GameData();
-        } else { 
-            rb = GetComponent<Rigidbody2D>();
+            gameData = saveSystem.LoadGame(); // Chargement de la sauvegarde
 
-            // SceneManager.LoadScene(gameData.playerLevel); // Mise à jour de la scène
-            rb.MovePosition(gameData.playerPosition); // Mise à jour de la position du joueur
+            if (gameData == null) { // Cas où aucune sauvegarde n'existe
+                gameData = new GameData();
+            } else { 
+                SceneManager.LoadScene(gameData.playerLevel); // Mise à jour de la scène
+                rb.MovePosition(gameData.playerPosition); // Mise à jour de la position du joueur
+            }
         }
     }
 
     void Update() {
-        if (rb != null) {
-            gameData.playerPosition = rb.position;
-        }
-    }
-
-    public void UpdateScene(string name) {
-        gameData.playerLevel = name;
+        gameData.playerPosition = rb.position;
+        gameData.playerLevel = SceneManager.GetActiveScene().name;
     }
 
     public void SaveGame()
