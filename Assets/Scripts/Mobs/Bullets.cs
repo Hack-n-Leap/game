ce1d191c932ae31f1c731a7652f381a4065abb20
Thotoman <aautordromero@gmail.com>
2024-05-27 21:09:00 +0200

    creation direction pour le tir

--- Assets/Scripts/Mobs/Bullets.cs
+++ Assets/Scripts/Mobs/Bullets.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+    public int dir = 1;
     public float speed;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * speed;
+        rb.velocity = new Vector2(dir * speed, rb.velocity.x);
         Destroy(gameObject,4f);
     }
 
