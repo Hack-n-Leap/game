3b87f698f60cb32020e14d2cf1a5ec238154a76b
ilo80 <ilann.mimet@gmail.com>
2024-05-28 13:03:18 +0200

    Modification keyBinding

--- Assets/Scripts/Windows/KeyBind.cs
+++ Assets/Scripts/Windows/KeyBind.cs
@@ -1,28 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class KeyBind : MonoBehaviour
 {
     private GameObject playerObject;
     private GameManager gameManager;
+    private TMP_Text buttonText;
+    private bool isBinding;
     public int functionIndex;
 
-    void Start()
+    private void Start()
     {
         playerObject = GameObject.FindWithTag("Player");
         gameManager = playerObject.GetComponent<GameManager>();
+        buttonText = gameObject.GetComponentInChildren<TMP_Text>();
+    }
+
+    private void Update() {
+        if (Input.anyKey) {
+            if (isBinding) {
+                isBinding = false;
+                foreach (KeyCode kc in System.Enum.GetValues(typeof(KeyCode))) {
+                    if (Input.GetKey(kc)) {
+                        gameManager.gameData.playerFunctionsKey[0] = kc;
+                        buttonText.text = kc.ToString();
+                        break;
+                    }
+                }
+            }
+
+        }
     }
 
     public void KeyBinding() {
-        if (Input.anyKey) {
-            foreach (KeyCode kc in System.Enum.GetValues(typeof(KeyCode))) {
-                if (Input.GetKey(kc)) {
-                    gameManager.gameData.playerFunctionsKey[0] = kc;
-                    break;
-                }
-            }
-        }
+        isBinding = !isBinding;
     }
 
 }
