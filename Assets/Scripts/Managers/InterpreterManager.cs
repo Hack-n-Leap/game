9eb35bc52e82cc87e2ab4b18ff45b5164b0f0943
ilo80 <ilo80.pro@gmail.com>
2024-08-08 14:04:15 +0200

    Ajout d'IronPython + Création class Interpreter

--- Assets/Scripts/Managers/InterpreterManager.cs
+++ Assets/Scripts/Managers/InterpreterManager.cs
@@ -13,11 +13,19 @@ public class InterpreterManager {
         InterpreterID = interpreterID;
     }
 
-    public void RunInterpreter (string code) {
+    public Interpreter RunCode (string code) {
         if (InterpreterID == 0)  {
-            Interpreter interpreter = new Interpreter();
+            PseudoCodeInterpreter.Interpreter pseudoCodeEngine = new PseudoCodeInterpreter.Interpreter();
 
             interpreter.EvaluateCode(code);
+        } else if (InterpreterID == 1) {
+            ScriptEngine pythonEngine = Python.CreateEngine();
+
+            pythonEngine.Execute(code)
         }
     }
+}
+
+public class Interpreter {
+    public int InterpreterID;
 }
